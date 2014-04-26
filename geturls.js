28a28fe3d99663f5a632a50b0c74feae7097eaee
jimarasim <jaemzware@hotmail.com>
2014-04-26 03:03:32 -0700

    Added ability to close all but the current tab, and update the list in the process.

--- geturls.js
+++ geturls.js
@@ -1,13 +1,78 @@
+var latestTabs;
+
+
+/**
+ * This is called when the document is loaded
+ * @param {type} param1
+ * @param {type} param2
+ */
 document.addEventListener('DOMContentLoaded', function () {
 
-    chrome.tabs.query({}, function (tabs) {
-        for (var i = 0; i < tabs.length; i++) {
-            console.log(tabs[i].url);
-            $("<li/>").appendTo("#urls").html("<u>"+tabs[i].title+"</u>");
-            $("<li/>").appendTo("#urls").html(tabs[i].url);
+    //generate the tab list
+    UpdateTabList();
+    
+    //add listener for when tabs are removed
+    chrome.tabs.onRemoved.addListener(function(tabId,removeInfo){
+            //remove all list items
+            $("#urls li").remove(); 
 
+            //re-do list
+            UpdateTabList();
+    });
+
+    
+    //create event to close all but the current tab
+    $("#closeAllButCurrent").click(function(){
+        CloseAllButCurrentTab();
+    });
+});
+
+/**
+ * This function builds the tab list in the default_popup
+ * @returns {undefined}
+ */
+function UpdateTabList(){
+    //query chrome for its tabs
+    chrome.tabs.query({}, function (tabs) {
+        
+        //cycle through all the tabs, and add them to the list in the default_popup
+        for (var i = 0; i < tabs.length; i++) {
+            
+            //append a list item for each tab.  one for the title, and on for the url
+            $("<li/>").appendTo("#urls").html("<b>"+tabs[i].title+"</b>");
+            
+            //url list item has a bottom margin, to space the entries apart
+            $("<li class='withmargin'/>").appendTo("#urls").html("<a id='"+i+"'>"+tabs[i].url+"</a>");
+            
+            //when the url is clicked, highlight the tab
+            $("#"+i).click(function(event){
+                chrome.tabs.highlight({tabs:[parseInt(event.target.id)]},function(window){/*console.log("highlight"+event.target.id);*/});
+                
+            });
         }
 
     });
-});
+}
+
+/**
+ * This function closes all but the current tab
+ * @returns {undefined}
+ */
+function CloseAllButCurrentTab(){
+    var currentTab;
+    chrome.tabs.query({ currentWindow: true, active: true },function(tabs) {
+        currentTab = tabs[0].id;
+        chrome.tabs.query({}, function (tabs) {
+            for (var i = 0; i < tabs.length; i++) {
+                if(tabs[i].id!=currentTab)
+                {
+                    chrome.tabs.remove(tabs[i].id, function(){});
+                }
+            }
+            
+            
+        });
+    });
+}
+
 
